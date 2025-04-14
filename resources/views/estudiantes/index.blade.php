359b5ea60060c32c11768547eff4a06f0c78fb1b
Chech3 <joseangelgarces123@gmail.com>
2025-04-14 16:25:52 -0400

    first commit

---
+++ resources/views/estudiantes/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1 class="text-white">Hola todos esta es la vista desde nativephp</h1>
+
+
+    <div class="flex flex-col items-center justify-center h-screen bg-gray-800">
+        <div class="h-48 w-48 bg-green-500 rounded-md">
+
+        </div>
+    </div>
+</body>
+
+</html>
